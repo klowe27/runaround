b17bd6d305d1d6f9fa258a3e904abc47105d0e09
Kristin Brewer-Lowe and Phil Mass <kristin@>
2019-01-14 11:04:54 -0800

    Pass enemy init spec

--- src/enemy.js
+++ src/enemy.js
@@ -1,6 +1,19 @@
 class Enemy {
-  constructor(){
+  constructor(type){
+    this.type = type;
+    this.x = 0;
+    this.y = 0;
+    this.life = 0;
+    this.lifeTotal = this.getTotalByType(type);
+  }
 
+  getTotalByType(type){
+    switch(type) {
+      case 0:
+        return 20;
+      default:
+        return 0;
+    }
   }
 }
 
