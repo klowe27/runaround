1f9da2686100ab02a49e94fe4d21f8047c70b772
Kristin Brewer-Lowe and Phil Mass <phil@>
2019-01-14 11:39:08 -0800

    Add failing spec for runaround init

--- spec/runaround-spec.js
+++ spec/runaround-spec.js
@@ -1,6 +1,12 @@
-// import { Runaround } from './../src/runaround.js';
-//
-// describe('Runaround', function() {
-//
-//
-// });
+import { Runaround } from './../src/runaround.js';
+
+describe('Runaround', function() {
+
+  it('should create a new instance of Runaround and store its properties', function() {
+    var runaround = new Runaround();
+    expect(runaround.level).toEqual(null);
+    expect(runaround.player).toEqual(null);
+    expect(runaround.bullets).toEqual([]);
+    expect(runaround.userInput).toEqual([]);
+  });
+});
